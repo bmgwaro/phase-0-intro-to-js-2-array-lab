db31e1bf67d848095b07536714d85d7d4834c079
bmgwaro <brian.gwaro@student.moringaschool.com>
2024-06-27 18:55:39 +0300

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats=["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(frank){
+    return [...cats, frank]
+}
+
+function prependCat(frank){
+    return [frank, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
